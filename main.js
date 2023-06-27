fa05a19e62ce750926460fc3564884ac0a767f5a
harkemoyo <markndeche91@gmail.com>
2023-06-27 07:13:31 +0300

    added countdown

--- main.js
+++ main.js
@@ -17,3 +17,81 @@ const finalTimeEl = document.querySelector(".final-time");
 const baseTimeEl = document.querySelector(".base-time");
 const penaltyTimeEl = document.querySelector(".penalty-time");
 const playAgainBtn = document.querySelector(".play-again");
+
+// equation
+let questionAmount = 0
+
+let equationArray = []
+
+// gamePage
+let firstNumber = 0
+let secondNumber = 0
+let equationObject = {}
+let wrongFormat = []
+
+// populate countdown start
+function countDownStart(){
+    countdown.textContent = '3'
+    setTimeout(()=>{
+        countdown.textContent = '2'
+    }, 500)
+    setTimeout(()=>{
+        countdown.textContent = '1'
+    }, 1000)
+    setTimeout(()=>{
+        countdown.textContent = 'G0!'
+    }, 1500)
+}
+
+// navigate from splash to countdown page
+function showCountDown(){
+    countdownPage.hidden = false
+    splashPage.hidden = true 
+    countDownStart()
+}
+
+
+// get value from selected radio button
+function getRadioValue(){
+    let radioValue;
+    radioInputs.forEach((inputInRadio)=>{
+        if (inputInRadio.checked) {
+            radioValue = inputInRadio.value   
+        }
+        
+    });
+    return radioValue
+}
+// form that decides amount of question
+function selectQuestionAmount(e) {
+ e.preventDefault()
+questionAmount = getRadioValue()
+    console.log("questionAmount:",questionAmount);
+    // check if there is a valid question amount
+    
+        showCountDown()
+    
+
+}
+
+
+
+startForm.addEventListener('click', () =>{
+    
+    // has queryselector that takes an array
+    radioContainers.forEach((radioEl)=>{
+        // to remove the selected styling
+        radioEl.classList.remove('selected-label')
+
+        // adding it back when if input is checked
+        if (radioEl.children[1].checked) {
+            radioEl.classList.add('selected-label')
+        }
+        
+    })
+    
+})
+
+// Event Listeners
+
+startForm.addEventListener('submit', selectQuestionAmount)
